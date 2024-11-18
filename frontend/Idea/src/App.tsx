bdca64ed5ce3722d5ca459beba301a2581baa566
“Cristoge” <cristo.g@outlook.es>
2024-11-18 17:34:38 +0100

    modelo estable

--- frontend/Idea/src/App.tsx
+++ frontend/Idea/src/App.tsx
@@ -4,55 +4,13 @@ import { getAuth, GithubAuthProvider, signInWithPopup } from "firebase/auth";
 import Login from './components/Login';
 import UserProfile from './components/EjemploInfo';
 import CreateProject from './components/AñadirProyecto';
+import LoginGitHub from './components/autenticacionGithub';
 function App() {
-  const auth = getAuth(app);
-  const provider = new GithubAuthProvider();
-
-  const githubSignUp = () => {
-    signInWithPopup(auth, provider)
-      .then(async (response) => {
-        const user = response.user;  
-        console.log(user);  
-
-        // obtiene el token
-        const idToken = await user.getIdToken();  
-        const credential = GithubAuthProvider.credentialFromResult(response);
-        const githubToken = credential ? credential.accessToken : null;
-        console.log("GithubToken:", githubToken);
-        console.log("ID Token:", idToken);
-
-
-        
-        try {
-          const backendResponse = await fetch('http://localhost:3000/users/github', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              idToken,
-              githubToken,
-            }),
-          });
-          const data = await backendResponse.json();
-          if (backendResponse.ok) {
-            console.log("Usuario agregado en el backend:", data);
-          } else {
-            console.error("Error al agregar el usuario:", data);
-          }
-        } catch (error) {
-          console.error("Error al enviar los datos al backend:", error);
-        }
-      })
-      .catch((error) => {
-        console.error("Error en el inicio de sesión con GitHub:", error);
-      });
-  };
+  
 
   return (
     <div className="auth-container">
-      <h1>Ejemplo 12 - Autenticación con GitHub</h1>
-      <button onClick={githubSignUp}>Iniciar sesión con GitHub</button>
+      <LoginGitHub />
       <Login />
       <UserProfile />
       <CreateProject />
